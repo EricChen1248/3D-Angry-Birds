d9b282f47017b01c367d369ffd46c809f61c07ef
Eric Chen <19816842+EricChen1248@users.noreply.github.com>
2017-12-15 17:41:34 +0800

    Corrected slingshot pouch static pointer from gameObject to script

--- Assets/Scripts/Classes/SlingshotPouch.cs
+++ Assets/Scripts/Classes/SlingshotPouch.cs
@@ -4,13 +4,13 @@ namespace Classes
 {
     internal class SlingshotPouch : MonoBehaviour
     {
+        public static SlingshotPouch Instance;
         public GameObject CurrentAmmo;
         public static readonly Vector3 StartingPosition = new Vector3(0, 2f, 0);
-        public static GameObject Instance;
 
         private void Start()
         {
-            Instance = gameObject;
+            Instance = this;
         }
 
         internal void Reset()
